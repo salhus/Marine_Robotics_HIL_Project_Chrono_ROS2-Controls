a880ecb9601589ae7184c2612da306bbbc64a126
Salman Husain <salman.husain@nlr.gov>
2026-06-08 14:10:40 -0600

    chore: gitignore colcon build/install/log directories
    These are generated by 'colcon build' and should not be tracked.

--- build/chrono_flap_sim/CMakeFiles/chrono_flap_sim_uninstall.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for chrono_flap_sim_uninstall.
--- build/hil_odrive_ros2_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shusain/projects/hil_odrive_chrono_ros2_control/src/hil_odrive_ros2_control
-# Build directory: /home/shusain/projects/hil_odrive_chrono_ros2_control/build/hil_odrive_ros2_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
